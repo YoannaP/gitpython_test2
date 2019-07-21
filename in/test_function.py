4b5219691d018d21bc0d9e6cbc6195bf4f40c786
YoannaP <ypeneva@gmail.com>
2019-07-21 12:57:55 +0300

    'in/out'

---
+++ in/test_function.py
@@ -0,0 +1,11 @@
+
+
+# path_in = "C:/MyDoc/Schroders/Year2/repos/in"
+path_out = "C:/MyDoc/Schroders/Year2/repos/out/"
+
+file = open(path_out + "dummy.txt", "w")
+
+file.write("hello!")
+
+
+file.close()
