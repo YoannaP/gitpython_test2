31042c83a607fc2521dd7d0d51ca215428178d80
YoannaP <ypeneva@gmail.com>
2019-07-21 17:30:16 +0300

    added output in branch

--- in/test_function.py
+++ in/test_function.py
@@ -1,9 +1,17 @@
-
+import git
 
 # path_in = "C:/MyDoc/Schroders/Year2/repos/in"
 path_out = "C:/MyDoc/Schroders/Year2/repos/out/"
 
+repo = git.Repo('C:/MyDoc/Schroders/Year2/repos')
+
+repo.git.checkout('-b', 'output_run2')
+
 file = open(path_out + "dummy.txt", "w")
+
+repo.git.add(A=True)
+repo.git.commit(m='added output in branch')
+repo.git.push('--set-upstream', 'origin', current)
 
 file.write("hello!")
 
